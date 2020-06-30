8e9c965152625b9b4a36009dd8a658d909504711
jaweed <jaweedshujauddin@gmail.com>
2020-06-30 23:34:24 +0500

    done

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { createAppContainer } from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack'
 
 import Splash from './src/screens/Splash/index'
-import GetStarted from './src/screens/GetStarted'
-import AddNumber from './src/screens/AddNumber'
+import GetStarted from './src/screens/GetStarted'    
+import AddNumber from './src/screens/AddNumber'     
 import EnterCode from './src/screens/EnterCode'
-import EnterEmail from './src/screens/EnterEmail'
+import EnterEmail from './src/screens/EnterEmail'      
 import EnterPassword from './src/screens/EnterPassword'
 import EnterName from './src/screens/EnterName'
 import Terms from './src/screens/Terms'      
